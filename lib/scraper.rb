5a3b9ad4611ee39abff86284ea90f2d6be6c8710
Justin Winger <justinwinger4@gmail.com>
2020-04-08 21:21:41 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -24,20 +24,20 @@ end
 end
 
 
-Scraper.new.get_page
+# Scraper.new.get_page
   
-  def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title && course.title != ""
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
-  end
+#   def print_courses
+#     self.make_courses
+#     Course.all.each do |course|
+#       if course.title && course.title != ""
+#         puts "Title: #{course.title}"
+#         puts "  Schedule: #{course.schedule}"
+#         puts "  Description: #{course.description}"
+#       end
+#     end
+#   end
   
-end
+# end
 
 
 
