cdf0c02784b366c91577d75893b2ef53c18eaf9a
Justin Winger <justinwinger4@gmail.com>
2020-04-08 21:08:04 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,8 +5,7 @@ require_relative './course.rb'
 
 class Scraper
   def get_page
-    html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses/")
-    doc = Nokogiri::HTML(html)
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
   
   def get_courses
